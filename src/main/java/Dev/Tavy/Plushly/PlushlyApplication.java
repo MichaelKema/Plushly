ace4568bf6ded83232fd863fbe69d64e7c193897
Octavia <mkkema02@gmail.com>
2025-05-19 10:52:44 -0400

    Initial commit

---
+++ src/main/java/Dev/Tavy/Plushly/PlushlyApplication.java
@@ -0,0 +1,13 @@
+package Dev.Tavy.Plushly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlushlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlushlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Dev/Tavy/Plushly/PlushlyApplicationTests.java
@@ -0,0 +1,13 @@
+package Dev.Tavy.Plushly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlushlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
