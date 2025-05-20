17219f100751834c5d8037fc098aab6d3f23ba7a
Octavia <mkkema02@gmail.com>
2025-05-20 13:47:16 -0400

    corrections

--- backend/src/main/java/Dev/Tavy/Plushly/repository/PlushieRepository.java
+++ backend/src/main/java/Dev/Tavy/Plushly/repository/PlushieRepository.java
@@ -1,10 +1,12 @@
 package Dev.Tavy.Plushly.repository;
 
-import Dev.Tavy.Plushly.model.Plushie;
-import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
-public class PlushieRepository extends  JpaRepository<Plushie,Long>{
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import Dev.Tavy.Plushly.model.Plushie;
+
+public interface  PlushieRepository extends  JpaRepository<Plushie,Long>{
     List<Plushie> findBySource(String source);
     List<Plushie> findBySourceIn(List<String> sources);
     
